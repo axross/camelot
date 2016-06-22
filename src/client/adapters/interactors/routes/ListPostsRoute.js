91cd16cb070eb3aa8a2e7c3caa7e7f7267660a8a
axross <me@axross.io>
2016-06-22 23:11:53 +0900

    Refactor ListPostsRoute

--- src/client/adapters/interactors/routes/ListPostsRoute.js
+++ src/client/adapters/interactors/routes/ListPostsRoute.js
@@ -1,26 +1,67 @@
 /* @flow */
+import bemmer from 'bemmer';
 import React from 'react';
+import { Link } from 'react-router';
+import BlogPost from '../../../../entities/BlogPost';
+import BlogPostCard from '../components/BlogPostCard';
+import GridBox from '../components/GridBox';
+
+type Props = {
+  className: ?string;
+}
 
 class ListPostsRoute extends React.Component {
-  constructor(props: Object) {
+  constructor(props: Props) {
     super(props);
+
+    this.state = {
+      blogPosts: [],
+    };
   }
 
+  state: {
+    blogPosts: Array<BlogPost>;
+  };
+
   componentDidMount() {
-    this.context.postsRepository.fetchAll();
+    this.context.blogPostsRepository.getAll()
+      .then(blogPosts => {
+        this.setState({
+          blogPosts,
+        });
+      });
   }
 
   render() {
+    const b = bemmer.createBuilder('listPostsRoute', this.props.className);
+
+    const blogPostCards = this.state.blogPosts.concat(this.state.blogPosts, this.state.blogPosts, this.state.blogPosts, this.state.blogPosts).map(blogPost => (
+      <Link
+        to={`/posts/${blogPost.slug}`}
+        key={blogPost.slug}
+      >
+        <BlogPostCard
+          className={b('__posts__item')}
+          blogPost={blogPost}
+        />
+      </Link>
+    ));
+
     return (
-      <div>
-        posts!
+      <div className={b()}>
+        <div className={b('__posts')}>
+          <GridBox>
+            {blogPostCards}
+          </GridBox>
+        </div>
       </div>
     );
   }
 }
 
 ListPostsRoute.contextTypes = {
-  postsRepository: React.PropTypes.any,
+  blogPostsRepository: React.PropTypes.any,
+  router: React.PropTypes.any,
 };
 
 export default ListPostsRoute;
