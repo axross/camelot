c11a1defc60f33ad5d2cb7a9350fbb84256167b2
axross <me@axross.io>
2016-06-22 23:06:27 +0900

    Modify style of authorProfile

--- src/client/adapters/interactors/components/AuthorProfile.js
+++ src/client/adapters/interactors/components/AuthorProfile.js
@@ -1,7 +1,11 @@
 import bemmer from 'bemmer';
 import React from 'react';
 
-const AuthorProfile = props => {
+type Props = {
+  className: ?string;
+};
+
+const AuthorProfile = (props: Props) => {
   const b = bemmer.createBuilder('authorProfile', props.className);
 
   return (
@@ -18,17 +22,13 @@ const AuthorProfile = props => {
         </div>
 
         <div className={b('__inner__bio')}>
-          Web Application Developer. <br />
-          I into React, Flowtype, Query Language, <br />
+          Web Application Developer.<br />
+          I into React, Flowtype, Query Language,<br />
           Application Architecture and Texas Hold'em.
         </div>
       </div>
     </div>
   );
-};
-
-AuthorProfile.propTypes = {
-  className: React.PropTypes.string,
 };
 
 export default AuthorProfile;
