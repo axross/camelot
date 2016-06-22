5fac004e1ed794c7bcd15ea00d43c99b0117315f
axross <me@axross.io>
2016-06-22 23:06:45 +0900

    Add style for BlogPostCard

--- src/client/adapters/interactors/components/BlogPostCard.js
+++ src/client/adapters/interactors/components/BlogPostCard.js
@@ -4,25 +4,33 @@ import React from 'react';
 import BlogPost from '../../../../entities/BlogPost';
 import RatioFixedPicture from './RatioFixedPicture';
 
-const BlogPostCard = props => {
+type Props = {
+  className: ?string;
+  blogPost: BlogPost;
+};
+
+const BlogPostCard = (props: Props) => {
   const b = bemmer.createBuilder('blogPostCard', props.className);
 
   return (
-    <div className={b()}>
+    <article className={b()}>
       <div className={b('__inner')}>
         <RatioFixedPicture
           className={b('__inner__thumbnail')}
           src={props.blogPost.thumbnailImageURL}
-          ratio={[8, 5]}
+          ratio={[2, 1]}
         />
-      </div>
-    </div>
-  );
-};
 
-BlogPostCard.propTypes = {
-  className: React.PropTypes.string,
-  blogPost: React.PropTypes.instanceOf(BlogPost).isRequired,
+        <h1 className={b('__inner__title')}>
+          {props.blogPost.title}
+        </h1>
+
+        <div className={b('__inner__publishedAt')}>
+          {props.blogPost.publishedAt.fromNow()}
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default BlogPostCard;
